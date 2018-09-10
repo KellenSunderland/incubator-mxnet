f27178747b004c0d3e9558690be387baca151607
Kellen Sunderland <kellen.sunderland@gmail.com>
2018-09-10 17:37:06 +0200

    Make sure g++ is installed

--- ci/docker/install/ubuntu_gcc8.sh
+++ ci/docker/install/ubuntu_gcc8.sh
@@ -20,4 +20,4 @@
 sudo add-apt-repository ppa:jonathonf/gcc-8.0
 sudo add-apt-repository ppa:jonathonf/gcc-7.3
 sudo apt-get update
-sudo apt-get install -y gcc-8
+sudo apt-get install -y gcc-8 g++-8
