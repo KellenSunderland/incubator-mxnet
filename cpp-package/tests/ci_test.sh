90492615b60a960b01efed486842c847e295fdfe
Kellen Sunderland <kellens@amazon.com>
2018-09-11 08:53:02 +0200

    Print correct parent dir

--- cpp-package/tests/ci_test.sh
+++ cpp-package/tests/ci_test.sh
@@ -17,7 +17,7 @@
 
 set -ex # exit on the first error
 cd $(dirname $(readlink -f $0))/../example
-echo $PWD
+echo `pwd`
 export LD_LIBRARY_PATH=$(readlink -f ../../lib):$LD_LIBRARY_PATH
 export LD_LIBRARY_PATH=$(readlink -f ../../build):$LD_LIBRARY_PATH
 echo $LD_LIBRARY_PATH
